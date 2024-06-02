127119764f1b841d1c5feb60762628b1ff0eff13
Pavel Titushkin <paveltitushkin@gmail.com>
2024-06-02 14:40:01 +0300

    Добавлен HybridLockHandler

---
+++ CustomSynchronizationPrimitives/SynchronizationPrimitives/HybridLockHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+
+namespace CustomSynchronizationPrimitives.SynchronizationPrimitives
+{
+    public class HybridLockHandler
+    {
+        private readonly object _locker = new object();
+        private SpinLockHandler _spinLockHandler = new SpinLockHandler();
+
+        public void Enter()
+        {
+            if (!Monitor.TryEnter(_locker))
+            {
+                //SpinLockEnter();
+                _spinLockHandler.Enter();
+            }
+        }
+
+        public void Exit()
+        {
+            if (Monitor.IsEntered(_locker))
+            {
+                Monitor.Exit(_locker);
+            }
+            else
+            {
+                //SpinLockExit();
+                _spinLockHandler.Exit();
+            }
+        }
+
+        public bool TryEnter(int timeout)
+        {
+            if (Monitor.TryEnter(_locker))
+            {
+                return true;
+            }
+            else
+            {
+                return _spinLockHandler.TryEnter(timeout);
+            }
+        }
+    }
+}
